cc19ee5853b9e58149d9e0a6cf7eee2387b8935a
Jahangir Alam <geostruc.samraat@gmail.com>
2021-11-23 18:35:48 +0600

    bookName ke console log a dekhano

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -3,7 +3,7 @@
         <h1 class="text-center">Todos App</h1> <hr />
 
 
-    <input type="text" name="name" placeholder="Enter book" /> <button type="button" class="btn btn-primary">Add</button>
+    <input type="text" v-model="bookName" name="name" placeholder="Enter book" /> <button @click="addBook" type="button" class="btn btn-primary">Add</button>
 
     <ul>
         <li v-for="(book, index) in bookList" :key="index"> {{index+1}}. {{book}} <button>Delete</button></li> 
@@ -17,11 +17,21 @@
 <script>
 export default {
     name: 'Todo',
+    data() {
+        return {
+            bookName: ''
+        }
+    },
 
     // computed property use korar karon hocche for loop a je logic dekhchi seta ke vejal na kore akhanei define kore diye shudhu use kora okk
     computed: {
         bookList() {
             return this.$store.state.bookList
+        }
+    },
+    methods: {
+        addBook() {
+            console.log(this.bookName)
         }
     },
 }
